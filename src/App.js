0b7defd1a96dc761946017d48cad4a2a6381366d
Ilona <hair.masterpiece@gmail.com>
2020-03-26 14:39:26 +0300

    Развернули проект

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import './App.module.scss';
+
+function App() {
+  return (
+    <div className="App">
+     <div className ={style.Item}>Item</div>;
+    </div>
+  );
+}
+
+export default App;
+
+
